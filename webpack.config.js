b2edc5b2c9e8480b714ca16bb88bc2b6dc02450f
satan-nigthmare <sumitsinghsakhwar@gmail.com>
2020-03-21 01:10:07 +0530

    Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Output Management'
+      }),
+  ],
   module: {
       rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader',
-              ]
-          },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ]
+        },
       ],
   },
 };
